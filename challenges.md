1e6b08c7754940cffb7a90c7343dbaa9ef165cd2
PHILIP <terhemeniorwua@gmail.com>
2026-07-10 23:45:03 +0100

    edit some features in the code

--- challenges.md
+++ challenges.md
@@ -179,4 +179,3 @@ This is about the difficulty of an early technical interview or a solid coding a
 get stuck, share your code rather than asking for the full solution, and I'll help you debug it step by step.
 
 
-
--- index.js
+++ index.js
@@ -3,42 +3,47 @@ let studentReg = [];
 
 class Student{
     constructor(name, age, course, scores){
-        this.Name = name
-        this.Age = age
-        this.Course = course
-        this.Scores = scores
-        this.Average = this.getAverage()
-        this.Grade = this.getGrade()
-        this.Passed = this.hasPassed()
+        this.name = name
+        this.age = age
+        this.course = course
+        this.scores = scores
+        this.average = this.getAverage()
+        this.grade = this.getGrade()
+        this.passed = this.hasPassed()
 
         studentReg.push(this)
     }
 }
 Student.prototype.addScore = function(addedScore){
-    this.Scores.push(addedScore)
+    this.scores.push(addedScore)
+
+    this.average =this.getAverage()
+    this.grade = this.getGrade()
+    this.passed= this.hasPassed()
+
 }
  Student.prototype.getAverage = function(){
-        let avr = (this.Scores).map(score => score)
-        .reduce((total, score) => total += score)/(this.Scores).length
+        let avr = (this.scores).map(score => score)
+        .reduce((total, score) => total += score)/(this.scores).length
         return avr
     }
 Student.prototype.getGrade = function(){
-        if(this.Average <= 40){
+        if(this.average <= 40){
            return 'F'
-        } else if(this.Average < 50){
+        } else if(this.average < 50){
            return 'D'
-        } else if(this.Average < 60){
+        } else if(this.average < 60){
            return 'C'
-        } else if(this.Average < 70){
+        } else if(this.average < 70){
            return 'B'
-        } else if(this.Average <=100){
+        } else if(this.average <=100){
             return 'A'
         }else{
            return'NIL'
         }
     }
 Student.prototype.hasPassed = function(){
-    if(this.Average >= 50){
+    if(this.average >= 50){
         return true
     } else{
         return false
@@ -56,9 +61,9 @@ let student1 = new Student('Philip', 23, 'Engineering', [34, 67, 86,100, 55])
 let student2 = new Student('Timothy', 23, 'Computer Science', [34, 27, 16,10, 35])
 let student3 = new Student('Joy', 23, 'Computer Science', [90, 87, 86,90, 55])
 let student4 = new Student('Hwee', 23, 'Computer Science', [100, 87, 86,90, 55])
-student2.addScore(50)
+student2.addScore(100)
 // student2.displayInfo('Timothy')
-// console.log(studentReg)
+console.log(studentReg)
 
 
 
@@ -66,55 +71,56 @@ student2.addScore(50)
 class School {
     constructor(schooName){
         this.schooName = schooName
-        this.studen =[]
+        this.students =[]
     }
     addStudents(student){
        for(let stud of studentReg){
-        if(stud.Name === student){
-            (this.studen).push(stud)
+        if(stud.name === student){
+            (this.students).push(stud)
         }
        }
     }
 
     removeStudent(stuName){
-         for(let stud of this.studen){
-        if(stud.Name === stuName){
-            (this.studen).pop(stuName)
+         for(let stud of this.students){
+        if(stud.name === stuName){
+            (this.students).pop(stuName)
         }
        }
     }
     findStudent(name){
-    return this.studen.find(student => {
-        if(student.Name === name){
+    return this.students.find(student => {
+        if(student.name === name){
             return student
         }
     })
     }
 
     topStudent(){
-      let maxAverage = Math.max(...this.studen.map(student => student.Average))
-      this.studen.forEach(student =>{
-        if(student.Average === maxAverage){
+      let maxAverage = Math.max(...this.students.map(student => student.average))
+      this.students.forEach(student =>{
+        if(student.average === maxAverage){
             console.log(student)
         }
       })
     }
 
     getPassedStudents(){
-        return this.studen.filter(student => student.Passed === true)
+        return this.students.filter(student => student.passed === true)
     }
 
     getStudentsByCourse(course){
-        return this.studen.filter(student => student.Course === course)
+        return this.students.filter(student => student.course === course)
     }
 
     schoolReport(){
+        let topStudent = this.topStudent()
 let report = `
 School: ${this.schooName}
-Total Students: ${this.studen.length}
+Total Students: ${this.students.length}
 Passed: ${this.getPassedStudents().length}
-Failed: ${this.studen.length - this.getPassedStudents().length}
-Top Student: 
+Failed: ${this.students.length - this.getPassedStudents().length}
+Top Student: ${topStudent.name}
         `
 
         return report
@@ -134,7 +140,7 @@ school.addStudents('Hwee')
 // school.topStudent()
 // console.log(school.getPassedStudents())
 // console.log(school.getStudentsByCourse('Computer Science'))
-console.log(school.schoolReport())
+// console.log(school.schoolReport())
 
 // console.log('==========================')
 // console.log(school)
