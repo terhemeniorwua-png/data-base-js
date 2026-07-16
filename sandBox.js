c97fd1759e58ced0f527128a44600f8a8cd0f908
PHILIP <terhemeniorwua@gmail.com>
2026-07-16 22:26:41 +0100

    edit password data type to string

--- sandBox.js
+++ sandBox.js
@@ -3,16 +3,16 @@
 
 
   let staffs = [
-        {name:'Joe', id:211, password:4443, class:'200', email:'joe@gmail.com'},
-        {name:'Mut', id:212, password:6390, class:'100', email:'joe@gmail.com'},
-        {name:'Mattew', id:213, password:4523, class:'300', email:'joe@gmail.com'},
-        {name:'Charles', id:214, password:4534, class:'400', email:'joe@gmail.com'},
+        {name:'Joe', id:'211', password:'4443', class:'200', email:'joe@gmail.com'},
+        {name:'Mut', id:'212', password:'6390', class:'100', email:'joe@gmail.com'},
+        {name:'Mattew', id:'213', password:'4523', class:'300', email:'joe@gmail.com'},
+        {name:'Charles', id:'214', password:'4534', class:'400', email:'joe@gmail.com'},
         
 // 
     ]
 
    let students = [
-        {name:'Tom', id:115, class:'500', email:'joe@gmail.com', performance:{}}
+        {name:'Tom', id:'115', class:'500', email:'joe@gmail.com', performance:{}}
     ]
 
     
@@ -20,8 +20,8 @@
 class Staff{
     constructor(name, id, pass){
         this.name = name
-        this.id = id
-        this.password = pass
+        this.id = String(id)
+        this.password = String(pass)
     }
 
      static signUp(name, id, pass){
@@ -79,15 +79,13 @@ class Staff{
 class Student{
     constructor(name, id, pass, clss, email){
         this.name = name
-        this.id = id
-        this.password = pass
+        this.id = String(id)
+        this.password = String(pass)
         this.class = clss
         this.email = email
         this.performance={}
 
 
-        
-        
     }
      static signUp(name, id, pass, clas, email){
         let student = students.find(student => student.id === id)
@@ -95,7 +93,7 @@ class Student{
              console.log('Student already exist, Login!')
             return
         }
-         let newStudent =new Student(name, id, pass, clas, email)
+         let newStudent = new Student(name, id, pass, clas, email)
          students.push(newStudent)
     }
     static logIn(id, pass){
@@ -116,16 +114,16 @@ class Student{
         console.log(student.performance)
     }
     changeInfo(id, oldPass, newPass){
-        let passLength = String(newPass)
 
-         let student = students.find(
+          let student = students.find(
             student => id === student.id && oldPass === student.password)
             if(!student){
                 console.log('Invalid login')
                 return;
             }
+        let passLength = String(newPass)
 
-            if(passLength.length != 4){
+            if(passLength.length !== 4){
                 console.log('Password must be 4 characters')
                 return;
             }
@@ -150,7 +148,7 @@ let student4 = Student.signUp('Wisdom', 102, 9012, '100', 'wisdom@gmail.com')
 
 let staff1= Staff.signUp('Phil', 101, 2324)
 // Staff.logIn(211, 4443)
-Staff.studentDisplay(211, '200')
+// Staff.studentDisplay(211, '200')
 // console.log(staffs)
 
 
@@ -161,9 +159,11 @@ Staff.studentDisplay(211, '200')
 
 
 // Staff.performanceUpdate(212, '100', 102, 70, 64, 70)
-Staff.performanceUpdate(211, '200', 101, 60, 64, 90)
+// Staff.performanceUpdate(211, '200', 101, 60, 64, 90)
 // Staff.performanceUpdate(213, '300', 103, 89, 54, 80)
 // Staff.performanceUpdate(214, '400', 102, 69, 74, 91)
 // Staff.performanceUpdate(215, '500', 105, 78, 54, 67)
 // Staff.performanceUpdate(214, '500', 104, 58, 54, 97)
-// Staff.studentDisplay(215, '500')
+// Staff.studentDisplay('215', '500')
+
+
